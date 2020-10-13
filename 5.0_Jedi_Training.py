42adb24ebd955bc96036abccd87ec46bc778f6fa
neyensian <neyensian@urbandaleschools.com>
2020-10-13 12:05:23 -0500

    asdf

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -1,25 +1,24 @@
-  #Sign your name:________________
-
+# Sign your name:Ian Neyens
+import random
 '''
  1. Make the following program work.
-   '''  
-     print("This program takes three numbers and returns the sum.")
-     total = 0
+'''
 
-     for i in range(3):
-         x = input("Enter a number: ")
-         total = total + i
-     print("The total is:", x)
-  
-
+# print("This program takes three numbers and returns the sum.")
+# total = 0
+#
+# for i in range(3):
+#     x = int(input("Enter a number: "))
+#     total += x
+# print("The total is:", total)
 
 '''
   2. Write a Python program that will use a FOR loop to print the even
      numbers from 2 to 100, inclusive.
 '''
 
-
-
+# for i in range(2, 101, 2):
+#     print(i)
 
 
 '''
@@ -28,9 +27,9 @@
      a WHILE loop, don't use a FOR loop.
 '''
 
-
-
-
+# for i in range(10, -1, -1):
+#     print(i)
+# print("Blast off")
 
 
 '''
@@ -38,8 +37,8 @@
 '''
 
 
-
-
+# num = random.randint(1, 10)
+# print(num)
 
 
 '''
@@ -52,3 +51,21 @@
      if statements.
       
 '''
+
+total = 0
+negative = 0
+zero = 0
+positive = 0
+for i in range(7):
+    i = int(input("Enter a number:"))
+    total += i
+    if i > 0:
+        positive += 1
+    elif i == 0:
+        zero += 1
+    else:
+        negative += 1
+print("The total sum was", total)
+print("The amount of negative numbers was", negative)
+print("The amount of times zero was put in was", zero)
+print("The amount of positive numbers was", positive)
--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -7,13 +7,18 @@ COIN TOSS PROGRAM
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
 
+import random
 
+heads = 0
+tails = 0
 
-
-
-
-
-
-
-
-
+for i in range(50):
+    N = random.randrange(2)
+    if N == 0:
+        print("Heads")
+        heads += 1
+    else:
+        print("Tails")
+        tails += 1
+print("Total Heads: ", heads)
+print("Total Tails: ", tails)
